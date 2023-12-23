8daf6fb450a791a88a2547e98c514252d3615cf2
Tengo <t596256969@gmail.com>
2023-12-23 08:37:12 +0400

    2569

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function tengo() {
+
+
+  return (
+    <>
+       
+      <div className="container">
+        <div className="lb1"><img src="../src/assets/tengo.svg" alt="" /></div>
+        <p className='p1'>Improve your front-end skills by building projects</p>
+        <p className='p2'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+       
+    </>
+  )
+}
+
+export default tengo
